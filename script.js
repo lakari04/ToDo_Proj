b0474fbd435b64c610016aff71b5b8b9090f120e
Jeratomb <rayen.phillip@googlemail.com>
2023-09-22 23:38:30 +0200

    Nav Bar with Buttons, Task-Container, Slideshow and footer

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const tasks = document.querySelectorAll(".task");
+let slideIndex = 0;
+showSlides();
 
-    tasks.forEach(task => {
-        task.addEventListener("dblclick", function() {
-            const taskDescription = task.querySelector(".task-description");
-            taskDescription.classList.toggle("completed");
-        });
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 8500); 
+}
+
+
+
+const accordionHeaders = document.querySelectorAll('.accordion-header');
+
+accordionHeaders.forEach(header => {
+    header.addEventListener('click', () => {
+        const accordionItem = header.parentElement;
+        const accordionContent = accordionItem.querySelector('.accordion-content');
+        
+        if (accordionContent.style.display === 'block') {
+            accordionContent.style.display = 'none';
+        } else {
+            accordionContent.style.display = 'block';
+        }
     });
 });
+
