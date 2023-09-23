ca0b25fa10f47f35ce0c50823f93d603f134a092
faouzi jertila <lakari@live.de>
2023-09-23 02:34:19 +0200

    some design changes

--- js/todo.js
+++ js/todo.js
@@ -1,20 +1,3 @@
-let slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 8500); 
-}
-
-
-
 const accordionHeaders = document.querySelectorAll('.accordion-header');
 
 accordionHeaders.forEach(header => {
